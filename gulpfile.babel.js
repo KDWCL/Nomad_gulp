ca08cc462103e31c8d9bc57cfd8f1d523004d539
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-05-29 13:44:57 +0900

    gulp 2 webserver

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 // 원래 commonjs 모듈을 사용하지만 es6 모듈 시스템을 사용하기 위해서는
 // 파일이름을 gulpfile.babel.js가 필요하고 .babelrc에 preset-env를 설정해준다.
 // @babel/core, @babel/register, @babel/preset-env 필요
@@ -18,6 +19,11 @@ const pug = () =>
 // pipe에 파일들을 넣고 변형한다고 생각.
 // gulp-pug 플러그인을 사용하여 pug template을 html로 변환시킴. gpug()
 
+const webserver = () =>
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+// https://www.npmjs.com/package/gulp-webserver <- 옵션확인
+// gulp 명령어 사용 시 바로 웹을 띄워주는 역할을 수행. gulp-connect도 있음.
+
 const clean = () => del(["build"]);
 // build 디렉토리 삭제
 
@@ -26,5 +32,7 @@ const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets]);
+const postDev = gulp.series([webserver]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
 // gulp dev를 하기 위해서는 export를 해줘야 한다.
