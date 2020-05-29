10fe3bfb3a3865a1a69ad9a664215e0a6dd493a5
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-05-29 14:32:47 +0900

    gulp 4 image

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
 // 원래 commonjs 모듈을 사용하지만 es6 모듈 시스템을 사용하기 위해서는
 // 파일이름을 gulpfile.babel.js가 필요하고 .babelrc에 preset-env를 설정해준다.
 // @babel/core, @babel/register, @babel/preset-env 필요
@@ -12,6 +13,10 @@ const routes = {
     // 모든 pug 파일들의 수정을 보고 싶기 때문에 watch경로를 새로 만들어서 설정해준다.
     src: "src/*.pug",
     dest: "build",
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
   },
 };
 // 함수 하나하나가 task임. task의 역할은 pug -> html 트랜스파일링, scss-> css 트랜스파일링 등 임.
@@ -30,19 +35,26 @@ const webserver = () =>
 const clean = () => del(["build"]);
 // build 디렉토리 삭제
 
-const watch = () => gulp.watch(routes.pug.watch, pug);
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+};
 // 계속 watch할 경로를 정해준다. watch: "src/**/*.pug"
 // 두번째 인자는 지정한 경로에 있는 파일이 변경되었을 때 어떤 task를 실행할지이다.
 
-const prepare = gulp.series([clean]);
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+// 이미지 최적화(용량 크기를 줄여준다.)
+
+const prepare = gulp.series([clean, img]);
 // series를 사용하면 task를 용도별로 정리할 수 있음.
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.parallel([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 // 두개의 task를 동시에 실행시키고 싶다면 parallel을 사용한다.
 // 원래 Starting -> Finished가 되고나서 다음 task가 Starting이 되어야 하는데
 // 두개가 동시에 Starting이 된다.
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const dev = gulp.series([prepare, assets, live]);
 // gulp dev를 하기 위해서는 export를 해줘야 한다.
